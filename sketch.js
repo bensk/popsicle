2ce975dcc86410ed7e574c1d2283bffccbc313eb
Ben Samuels-Kalow <bensamuelskalow@gmail.com>
2016-09-26 13:32:28 -0400

    :lollipop: AP classes

--- sketch.js
+++ sketch.js
@@ -5,6 +5,8 @@ var c_801 = [ "Geehaan", "Jerry", "Ashley", "Shaynne", "Jesus", "Brindy", "Beyon
 var c_804 = ["Adrianna", "Alexander", "Alyssa", "Axel", "Brianna", "Caitlin", "Chanel", "Chasity", "Crystal", "Dallana", "Devonte", "Fatoumata", "Jaron", "Jeremiah", "Jese", "Joncarlo", "Josue", "Keisha", "Kendra", "Luis", "Mariam", "Mariyam", "Matthew", "Mayra", "Melvin", "Nyasia", "Ricardo", "Tanisha", "Xiuya", "Yafer", "Yaritza"]
 var econ = ["Anthony", "Bianny", "Charleen", "Christopher", "Darlenis", "Dazhane", "Elias", "Elvis", "Hailey", "Hector", "Jennifer", "Joanna", "Jonathan", "Jose", "Joshua", "Julian", "Julio", "Kellam", "Marlene", "Nephi", "Yennifer"]
 var cs_11 = ["Aaron", "Aileen", "Angie", "Bestydanis", "Brionna", "David", "Dayris", "Genesis", "Gregorio", "Grismely", "Ibrahim", "Izaiyah", "James", "Jorge", "Jose", "Justin", "Karen", "Luis", "Marquis", "Michael Chavez", "Michael Delvalle", "Mommina", "Steven", "Vianelys", "Yomaris", "Zaire"]
+
+var ap =["TiJuan", "Dayris", "Michael F.", "James", "Michael DV", "Brionna", "Luis", "David", "Karen", "Jose", "Izaiyah", "Genesis", "Grismely", "Aaron", "Ibrahim", "Mommina", "Aileen", "Gissel", "Vianelys", "Bestydanis", "Angie", "Jorge", "Justin", "Zaire", "Steven", "Terrell", "Gregorio", "Yomaris"]
     // var index;
     // Get a random element from an array
 
@@ -25,6 +27,9 @@ function setup() {
     button804 = createButton("804")
     button804.position(windowWidth / 2+140, 10);
     // button804.touchEnded(popsicle804);
+
+    buttonAP = createButton("AP")
+    buttonAP.position(windowWidth/2-30,280)
     touchStarted();
     touchEnded();
 
@@ -32,6 +37,7 @@ function setup() {
         button801.touchStarted(popsicle801);
         button803.touchStarted(popsicle803);
         button804.touchStarted(popsicle804);
+        buttonAP.touchStarted(popsicleAP)
 
     }
 
@@ -76,6 +82,19 @@ function popsicle804() {
     textFont("Fugaz One")
 
     text(String(c_804[floor(random(c_801.length))]), windowWidth / 2, windowHeight / 2); // Displays one of the four words
+
+}
+
+function popsicleAP() {
+    background('white')
+        // line(0, 0, 50, 50)
+        // text(words[1], 10, 50)
+    textSize(72)
+    textAlign(CENTER)
+
+    textFont("Fugaz One")
+
+    text(String(ap[floor(random(ap.length))]), windowWidth / 2, windowHeight / 2); // Displays one of the four words
 
 }
 
